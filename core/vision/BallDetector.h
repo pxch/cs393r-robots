bdd7bf1ba91eb1b80c0991f12d0eb5a17b897a41
Tianyang Li <ty@li-tianyang.com>
2013-09-07 18:11:00 -0500

    updated cpp frame for ball detection

--- core/vision/BallDetector.h
+++ core/vision/BallDetector.h
@@ -7,16 +7,27 @@
 #include <vision/Classifier.h>
 #include <vision/structures/BallCandidate.h>
 
-class BallDetector : public ObjectDetector {
- public:
-  BallDetector(DETECTOR_DECLARE_ARGS, Classifier*& classifier, BlobDetector*& blob_detector);
-  void init(TextLogger* tl){textlogger = tl;};
-  BallCandidate candidates[MAX_BALL_CANDS];
-  int candidateCount;
- private:
-  TextLogger* textlogger;
-  Classifier* classifier_;
-  BlobDetector* blob_detector_;
+class BallDetector: public ObjectDetector {
+public:
+	BallDetector(DETECTOR_DECLARE_ARGS, Classifier*& classifier,
+			BlobDetector*& blob_detector);
+
+	void init(TextLogger* tl) {
+		textlogger = tl;
+	}
+
+	void detectBall();
+
+	BallCandidate candidates[MAX_BALL_CANDS];
+
+	int candidateCount;
+
+private:
+	TextLogger* textlogger;
+
+	Classifier* classifier_;
+
+	BlobDetector* blob_detector_;
 };
 
 #endif
--- core/vision/BlobDetector.cpp
+++ core/vision/BlobDetector.cpp
@@ -1,10 +1,14 @@
 #include "BlobDetector.h"
 
 BlobDetector::BlobDetector(DETECTOR_DECLARE_ARGS, Classifier*& classifier) :
-  DETECTOR_INITIALIZE, classifier_(classifier) {
-  for(int i = 0; i < NUM_COLORS; i++)
-    horizontalBlob.push_back(std::vector<Blob>());
+		DETECTOR_INITIALIZE, classifier_(classifier) {
+	for (int i = 0; i < NUM_COLORS; i++)
+		horizontalBlob.push_back(std::vector<Blob>());
 
-  for(int i = 0; i < NUM_COLORS; i++)
-    verticalBlob.push_back(std::vector<Blob>());
+	for (int i = 0; i < NUM_COLORS; i++)
+		verticalBlob.push_back(std::vector<Blob>());
 }
+
+void BallDetector::detectBall() {
+}
+
