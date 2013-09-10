bbf960c009688002ab03e0a42a2edaf0314b823d
Tianyang Li <ty@li-tianyang.com>
2013-09-09 19:55:17 -0500

    changed head tilt

--- core/python/testFSM.py
+++ core/python/testFSM.py
@@ -12,7 +12,7 @@ class TestMachine(StateMachine):
     walk = WalkNode()
     locateBall = LocateBallNode()
     locateBlueWall = LocateBlueWallNode()
-    tiltHead = TiltHeadNode(-21.1)
+    tiltHead = TiltHeadNode(-21)
     walkLeft = WalkLeftNode()
     walkRight = WalkRightNode()
 
