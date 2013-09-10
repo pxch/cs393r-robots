e42b34e92a8248feddb8cbd3c55e91ae6e8fa495
Tianyang Li <ty@li-tianyang.com>
2013-09-09 19:57:52 -0500

    changed head tilit to 21.2

--- core/python/testFSM.py
+++ core/python/testFSM.py
@@ -12,7 +12,7 @@ class TestMachine(StateMachine):
     walk = WalkNode()
     locateBall = LocateBallNode()
     locateBlueWall = LocateBlueWallNode()
-    tiltHead = TiltHeadNode(-21)
+    tiltHead = TiltHeadNode(-21.2)
     walkLeft = WalkLeftNode()
     walkRight = WalkRightNode()
 
