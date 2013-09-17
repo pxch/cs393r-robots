0f54985e550fe7f805cea48be62723f6528f7e28
Tianyang Li <ty@li-tianyang.com>
2013-09-16 20:43:13 -0500

    test find ball

--- core/python/testFSM.py
+++ core/python/testFSM.py
@@ -14,12 +14,12 @@ class TestMachine(StateMachine):
     searchGoal = SearchGoalNode()
     kickBall = KickBallNode()
 
-    self._adt(start, N, stand, C, TiltHeadNode(-26.5), S, finish)
-    # self._adt(stand, C, searchBall)
-    # self._adt(searchBall, S, searchGoal)
-    # self._adt(searchGoal, S, kickBall)
-    # self._adt(kickBall, S, sit)
-    # self._adt(sit, C, finish)
+    self._adt(start, N, stand)
+    self._adt(stand, C, searchBall)
+    self._adt(searchBall, S, searchGoal)
+    self._adt(searchGoal, S, kickBall)
+    self._adt(kickBall, S, sit)
+    self._adt(sit, C, finish)
 
 class SearchBallNode(Node):
   MY_SUCCESS = 0
