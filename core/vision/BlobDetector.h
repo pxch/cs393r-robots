b51c84fbac240e22fbe64ee435a0b612f407aeeb
Jacob Menashe <jmenashe@cs.utexas.edu>
2013-09-18 20:44:40 -0500

    Added blob detection.

--- core/vision/BlobDetector.h
+++ core/vision/BlobDetector.h
@@ -3,21 +3,29 @@
 
 
 #include <memory/TextLogger.h>
+#include <constants/VisionConstants.h>
 #include <vision/Classifier.h>
 #include <vision/structures/Blob.h>
+#include <vision/structures/VisionParams.h>
+#include <vision/Macros.h>
 #include <vision/ObjectDetector.h>
-#include <vision/structures/Blob.h>
-
-typedef std::vector<Blob> BlobCollection;
+#include <vision/enums/Colors.h>
   
+typedef std::vector<Blob> BlobCollection;
+
 class BlobDetector : public ObjectDetector {
  public:
   BlobDetector(DETECTOR_DECLARE_ARGS, Classifier*& classifier);
   void init(TextLogger* tl){textlogger = tl;};
   std::vector<BlobCollection> horizontalBlob, verticalBlob;
+  void formWhiteLineBlobs();
+  void formBlobs(Color color);
+
  private:
+  Classifier*& classifier_;
+  VisionPoint ***verticalPoint, ***horizontalPoint;
+  uint32_t **verticalPointCount, **horizontalPointCount;
   TextLogger* textlogger;
-  Classifier* classifier_;
 };
 
 #endif
--- core/vision/Classifier.h
+++ core/vision/Classifier.h
@@ -28,26 +28,39 @@ class Classifier {
   uint32_t **horizontalPointCount, **verticalPointCount;
 
   bool classifyImage(unsigned char*);
+
+  void constructRuns(int colorFlags = ~0);
+  bool didHighResBallScan;
+
+
   void setStepScale(int,int);
   void getStepSize(int&,int&);
   void getStepScale(int&,int&);
+  inline Color xy2color(int x, int y) {
+    return (Color)segImg_[y * iparams_.width + x];
+  }
  private:
-  void classifyImage(const std::vector<FocusArea>& areas, unsigned char*);
-  void classifyImage(const FocusArea& area, unsigned char*);
-  void clearPoints(int colorFlags = ~0);
-
-  bool setImagePointers();
-  
   const VisionBlocks& vblocks_;
   const VisionParams& vparams_;
   const ImageParams& iparams_;
   const Camera::Type& camera_;
+  bool initialized_;
+  bool doingHighResScan_;
+  bool bodyExclusionAvailable;
   TextLogger* textlogger;
+
+  // For excluding parts of the shoulder in the body
+  float bodyExclusionSlope[NUM_BODY_EXCL_POINTS];
+  float bodyExclusionOffset[NUM_BODY_EXCL_POINTS];
 
   unsigned char* img_;
   unsigned char* segImg_, *segImgLocal_;
   uint16_t vstep_, hstep_, vscale_, hscale_;
+  HorizonLine horizon_;
   unsigned char* colorTable_;
-  bool initialized_;
+  bool* pointScanned;
+  uint16_t* vGreenPosition;
+  uint16_t* hGreenPosition;
+  bool fromLog_;
 };
 #endif
