ce29138c0756f8dfe805bec1d1b927cfc4d8b734
Tianyang Li <ty@li-tianyang.com>
2013-09-25 20:03:16 -0500

    test compile

--- core/vision/BallTrack.h
+++ core/vision/BallTrack.h
@@ -7,6 +7,18 @@ class BallTracker {
 public:
 	BallTracker(float dT_) :
 			tracking(false), dT(dT_) {
+
+		for (int i = 0; i != 4; ++i) {
+			for (int j = 0; j != 4; ++j) {
+				A(i, j) = 0.0;
+			}
+		}
+		for (int i = 0; i != 4; ++i) {
+			A(i, i) = 1.0;
+		}
+		A(0, 2) = dT_;
+		A(1, 3) = dT_;
+
 	}
 
 	~BallTracker() {
@@ -24,6 +36,8 @@ public:
 	 * v_y
 	 */
 	Eigen::Vector4d state;
+
+	Eigen::Matrix4f A;
 
 	float const dT; /* time interval between 2 observations */
 
