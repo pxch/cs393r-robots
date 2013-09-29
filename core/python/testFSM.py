77ebd4690ef841c75f7e2cc51af828dcf069134a
Tianyang Li <ty@li-tianyang.com>
2013-09-28 22:11:50 -0500

    test not wlaking into white lines

--- core/python/testFSM.py
+++ core/python/testFSM.py
@@ -10,12 +10,59 @@ class TestMachine(StateMachine):
     finish = Node()
     sit = SitNode()
     stand1 = StandNode()
+    testWhiteLine = RandWalkGoalLine()
 
-    self._adt(start, N, TiltHeadNode(-26.5), C, stand1, C, finish)
+    self._adt(start, N, TiltHeadNode(-26.5), C, stand1, C, testWhiteLine)
 
 class RandWalkGoalLine(Node):
+  MY_START = 0
+  
+  MY_WALK = 1
+  
+  MY_SWITCH = 2
+  
+  WALK_FRAMES = 100
+  
   def __init__(self):
-    super(RandWalkGoalLine, self).__init__()    
+    super(RandWalkGoalLine, self).__init__()   
+    self.myState = RandWalkGoalLine.MY_START
+    self.walkCount = 0
+    
+    # walk parameters 
+    self.x = 0.0
+    self.y = 0.0
+  
+  def run(self):
+    if self.myState == RandWalkGoalLine.MY_START:
+      commands.stand()
+      self.myState = RandWalkGoalLine.MY_SWITCH
+      
+    elif self.myState == RandWalkGoalLine.MY_SWITCH:
+      randDir = randint(1, 4)
+      if randDir == 1:
+        self.x = 0.5
+        self.y = 0.0
+      elif randDir == 2:
+        self.x = -0.5
+        self.y = 0.0
+      elif randDir == 3:
+        self.x = 0.0
+        self.y = 0.5
+      elif randDir == 4:
+        self.x = 0.0
+        self.y = -0.5
+      self.myState = RandWalkGoalLine.MY_WALK
+      
+    elif self.myState == RandWalkGoalLine.MY_WALK:
+      whiteLine = core.world_objects.getObjPtr(core.WO_OPP_GOAL_LINE)
+      if whiteLine.fieldLineIndex == 0:
+        commands.setWalkVelocity(self.x, self.y, 0.0)
+      else:
+        commands.stand()
+      self.walkCount += 1
+      if self.walkCount == RandWalkGoalLine.WALK_FRAMES:
+        self.walkCount = 0
+        self.myState = RandWalkGoalLine.MY_SWITCH
 
 class SpeakNode(Node):
   def __init__(self, phrase):
