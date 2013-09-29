eb04afab4e2858055c5fb71c78ef101f4361cda8
Tianyang Li <ty@li-tianyang.com>
2013-09-28 22:18:38 -0500

    print more info

--- core/python/testFSM.py
+++ core/python/testFSM.py
@@ -59,8 +59,10 @@ class RandWalkGoalLine(Node):
       whiteLine = core.world_objects.getObjPtr(core.WO_OPP_GOAL_LINE)
       if whiteLine.fieldLineIndex == 0:
         commands.setWalkVelocity(self.x, self.y, 0.0)
+        print "white line seen"
       else:
         commands.stand()
+        print "white line not seen"
       self.walkCount += 1
       if self.walkCount == RandWalkGoalLine.WALK_FRAMES:
         self.walkCount = 0
