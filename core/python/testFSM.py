bc83c6bbfa5500a6ee16b91a6bce608e76d1943b
Tianyang Li <ty@li-tianyang.com>
2013-09-30 21:01:44 -0500

    do not print in python

--- core/python/testFSM.py
+++ core/python/testFSM.py
@@ -34,7 +34,6 @@ class GoalieNode(Node):
     
     if self.myState == GoalieNode.MY_WAIT_FOR_BALL_TO_MOVE:
       ball = core.world_objects.getObjPtr(core.WO_BALL)
-      print ball.ballBlobIndex
       if ball.ballBlobIndex != 0:
         self.myState = GoalieNode.MY_BLOCK_BALL
     
