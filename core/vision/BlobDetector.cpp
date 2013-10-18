1fe839ae937a6001714d4dfa19c5894063fd0aaa
Pengxiang Cheng <pxcheng@cs.utexas.edu>
2013-10-17 20:31:28 -0500

    change param

--- core/vision/BlobDetector.cpp
+++ core/vision/BlobDetector.cpp
@@ -18,10 +18,7 @@ bool BlobDetector::isOverlapped(VisionPoint& a, VisionPoint& b) {
 void BlobDetector::mergeBlob(BlobCollection& blobs, int color, int indexA,
 		int indexB) {
 	//merge A into B, mark A as invalid
-//	blobs[indexB].lpCount += blobs[indexA].lpCount;
-//	for (int i = 0; i < blobs[indexA].lpCount; ++i) {
-//		blobs[indexB].lpIndex.push_back(blobs[indexA].lpIndex[i]);
-//	}
+
 	for (uint16_t i = 0; i < blobs[indexA].lpCount; ++i) {
 		uint32_t pointIndex = blobs[indexA].lpIndex[i];
 		addPointToBlob(
@@ -58,6 +55,7 @@ void BlobDetector::addPointToBlob(VisionPoint& point, uint32_t pointIndex,
 				/ currentPixelCount;
 	}
 	blob.correctPixelCount = currentPixelCount;
+	blob.correctPixelRatio = blob.correctPixelCount / (blob.dx * blob.dy);
 }
 
 void BlobDetector::formBlobs(int color) {
@@ -69,11 +67,9 @@ void BlobDetector::formBlobs(int color) {
 	uint32_t pointIndex = 0;
 	uint32_t pointLine = 0;
 	uint32_t pointColumn = 0;
-//	Blob* newBlob;
 
 //To do
 	uint16_t blobIndex = 0;
-//	int currentBlobIndex = 0;
 	for (uint32_t x = 0; x < horizontalPointCount[0]; ++x) {
 		pointIndex = x;
 
@@ -82,19 +78,6 @@ void BlobDetector::formBlobs(int color) {
 				currentBlobs[blobIndex], blobIndex);
 		++blobIndex;
 
-//			newBlob = new Blob;
-//			newBlob->lpCount = 1;
-//			newBlob->lpIndex[0] = pointIndex;
-//			newBlob->invalid = false;
-//			newBlob->xi = 0;
-//			newBlob->yi = 0;
-//			newBlob->xf = iparams_.width - 1;
-//			newBlob->yf = iparams_.height - 1;
-//			newBlob->avgX = 0;
-//			newBlob->avgY = 0;
-//			newBlob->correctPixelRatio = 0.0;
-//
-//			currentBlobs.push_back(newBlob);
 	}
 	for (int y = 1; y < iparams_.height; ++y) {
 		for (uint32_t x = 0; x < horizontalPointCount[y]; ++x) {
@@ -107,34 +90,12 @@ void BlobDetector::formBlobs(int color) {
 				}
 			}
 			if (xx < horizontalPointCount[y - 1]) {
-//				currentBlobIndex = horizontalPoint[y - 1][xx].lbIndex;
-//				horizontalPoint[y][x].lbIndex = currentBlobIndex;
-//				++currentBlobs[currentBlobIndex].lpCount;
-//				currentBlobs[currentBlobIndex].lpIndex[currentBlobs[currentBlobIndex].lpCount
-//						- 1] = pointIndex;
+
 				blobIndex = horizontalPoint[y - 1][xx].lbIndex;
 				addPointToBlob(horizontalPoint[y][x], pointIndex,
 						currentBlobs[blobIndex], blobIndex);
 			} else {
-//				horizontalPoint[y][x].lbIndex = blobCount++;
 
-//				currentBlobs.push_back(
-//						Blob(iparams_.width - 1, 0, iparams_.height - 1, 0,
-//								pointIndex));
-
-//					newBlob = new Blob;
-//					newBlob->lpCount = 1;
-//					newBlob->lpIndex[0] = pointIndex;
-//					newBlob->invalid = false;
-//					newBlob->xi = 0;
-//					newBlob->yi = 0;
-//					newBlob->xf = iparams_.width - 1;
-//					newBlob->yf = iparams_.height - 1;
-//					newBlob->avgX = 0;
-//					newBlob->avgY = 0;
-//					newBlob->correctPixelRatio = 0.0;
-//
-//					currentBlobs.push_back(newBlob);
 				blobIndex = currentBlobs.size();
 				currentBlobs.push_back(Blob());
 				addPointToBlob(horizontalPoint[y][x], pointIndex,
@@ -160,7 +121,7 @@ void BlobDetector::formBlobs(int color) {
 		}
 	}
 
-	// Mark blob as invalid if area lower than 100
+	// Mark blob as invalid if length lower than 20 or ratio lower than 0.5
 	int blobSize = 0;
 
 	for (unsigned int i = 0; i < currentBlobs.size(); ++i) {
@@ -178,64 +139,21 @@ void BlobDetector::formBlobs(int color) {
 	uint16_t xi_1, xi_2, xf_1, xf_2, yi_1, yi_2, yf_1, yf_2, dx_2, dy_2;
 	for (unsigned int i = 0; i < currentBlobs.size(); ++i) {
 		if (currentBlobs[i].invalid == false) {
-//				Blob validBlob = currentBlobs[i];
-//				horizontalBlob[c].push_back(Blob(currentBlobs[i]));
-//				++blobCount;
+
 			horizontalBlob[color][blobIndex++] = currentBlobs[i];
 			for (uint16_t j = 0;
 					j < horizontalBlob[color][blobIndex - 1].lpCount; ++j) {
-//				pointIndex = horizontalBlob[color][blobCount - 1].lpIndex[j];
-//				pointLine = pointIndex >> 16;
-//				pointColumn = pointIndex & 0xfffful;
 
 				horizontalPoint[pointLine][pointColumn].lbIndex = blobIndex;
 
-//				xi_1 = horizontalBlob[color][blobCount - 1].xi;
-//				xf_1 = horizontalBlob[color][blobCount - 1].xf;
-//				yi_1 = horizontalBlob[color][blobCount - 1].yi;
-//				yf_1 = horizontalBlob[color][blobCount - 1].yf;
-//
-//				xi_2 = horizontalPoint[pointLine][pointColumn].xi;
-//				xf_2 = horizontalPoint[pointLine][pointColumn].xf;
-//				yi_2 = horizontalPoint[pointLine][pointColumn].yi;
-//				yf_2 = horizontalPoint[pointLine][pointColumn].yf;
-//
-//				dx_2 = horizontalPoint[pointLine][pointColumn].dx;
-//
-//				horizontalBlob[color][blobCount - 1].xi =
-//						xi_1 > xi_2 ? xi_2 : xi_1;
-//				horizontalBlob[color][blobCount - 1].yi =
-//						yi_1 > yi_2 ? yi_2 : yi_1;
-//
-//				horizontalBlob[color][blobCount - 1].xf =
-//						xf_1 > xf_2 ? xf_1 : xf_2;
-//				horizontalBlob[color][blobCount - 1].yf =
-//						yf_1 > yf_2 ? yf_1 : yf_2;
-//
-//				horizontalBlob[color][blobCount - 1].avgX += (xi_2 + xf_2) / 2
-//						* dx_2;
-//
-//				horizontalBlob[color][blobCount - 1].avgY += yi_2 * dx_2;
-//
-//				horizontalBlob[color][blobCount - 1].correctPixelCount += dx_2;
 			}
-//			if (horizontalBlob[color][blobCount - 1].correctPixelCount > 0) {
-//				horizontalBlob[color][blobCount - 1].avgX /=
-//						horizontalBlob[color][blobCount - 1].correctPixelCount;
-//				horizontalBlob[color][blobCount - 1].avgY /=
-//						horizontalBlob[color][blobCount - 1].correctPixelCount;
-//			}
-//			horizontalBlob[color][blobCount - 1].dx =
-//					horizontalBlob[color][blobCount - 1].xf
-//							- horizontalBlob[color][blobCount - 1].xi + 1;
-//			horizontalBlob[color][blobCount - 1].dy =
-//					horizontalBlob[color][blobCount - 1].yf
-//							- horizontalBlob[color][blobCount - 1].yi + 1;
+
 			std::cout << color << "[ " << blobIndex << " ]: "
 					<< horizontalBlob[color][blobIndex - 1].xi << ", "
 					<< horizontalBlob[color][blobIndex - 1].yi << ", "
 					<< horizontalBlob[color][blobIndex - 1].xf << ", "
-					<< horizontalBlob[color][blobIndex - 1].yf << std::endl;
+					<< horizontalBlob[color][blobIndex - 1].yf << ", "
+					<< horizontalBlob[color][blobIndex - 1].correctPixelRatio << std::endl;
 		}
 	}
 }
