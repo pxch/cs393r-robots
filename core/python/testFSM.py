2d7718eecbe341ce86a05f28f6aa07295e748454
lijia <lijia@lijia-ThinkPad-T430.(none)>
2013-10-29 22:06:02 -0500

    change testFSM

--- core/python/testFSM.py
+++ core/python/testFSM.py
@@ -1,5 +1,5 @@
 from state import * 
-import commands, core, util, pose, percepts
+import commands, core, util, pose
 import time
 from math import pi
 
@@ -8,70 +8,118 @@ class TestMachine(StateMachine):
     start = Node()
     finish = Node()
     sit = SitNode()
-    stand1 = StandNode()
-    stand2 = StandNode()
-    walk = WalkNode(1, 0.1, 0, 0)
-    locateBall = LocateBallNode(False)
-    locateBlueWall = LocateBlueWallNode()
-    tiltHead = TiltHeadNode(-21.1)
-    walkLeft = WalkLeftNode(0.1)
-    walkRight = WalkRightNode(0.1)
+    stand = StandNode()
+    choose = ChooseNode()
+    self._adt(start, N, stand)
+    self._adt(stand, C, choose)
+    self._adt(choose, S(Choices.Forward), WalkNode(), S, choose)
+    self._adt(choose, S(Choices.Left), TurnLeftNode(), S, choose)
+    self._adt(choose, S(Choices.Right), TurnRightNode(), S, choose)
+    self._adt(choose, I(4), SpeakNode('completed'), S, sit)
+    self._adt(sit, C, finish)
 
-    self._adt(start, N, stand1, C, finish)
-    #self._adt(stand1, C, WalkNode(10.0, 0.1, 0, 0), S, WalkNode(5.0, 0, 0, pi / 4), S, StandNode(), C, TurnHeadNode(-pi / 9, 2.0, True), S, TurnHeadNode(pi / 9, 2.0, True), S, sit, C, stand2)
-    #self._adt(stand2, C, tiltHead)
-    #self._adt(tiltHead, C, locateBall)
-    #self._adt(locateBall, S(BallLocation.Left), walkLeft, S, locateBall)
-    #self._adt(locateBall, S(BallLocation.Right), walkRight, S, locateBall)
-    #self._adt(locateBall, S(BallLocation.Middle), walk, S, locateBall)
 
-class BallLocation:
+
+#######################################assignment5##########################
+class TestMachine5(StateMachine):
+  def setup(self):
+    start = Node()
+    finish = Node()
+    sit = SitNode()
+    stand = StandNode()
+    walk = WalkNode()
+    walk1 = towardsToCenterNode()
+    self._adt(start, N, stand)
+    self._adt(stand, C, walk)
+    self._adt(walk, C, sit)
+    self._adt(sit, C, finish)
+
+
+class towardsToCenterNode(Node):
+  def __init__(self):
+    super(towardsToCenterNode, self).__init__()
+
+  def run(self):
+    robot = core.world_objects.getObjPtr(core.robot_state.WO_SELF)
+    
+    if abs(robot.loc.x) < 50 and abs(robot.loc.y) < 50:
+      self.postCompleted()
+
+    elif robot.loc.x >=0 and robot.loc.y >=0:
+      if abs(tan(robot.orientation) - y/x) < 0.1 and robot.orientation >= 180 and robot.orientation <= 270:
+        commands.setWalkVelocity(0.5, 0, 0) 	      
+      else:
+	commands.setWalkVelocity(0, 0, -0.25)
+
+    elif robot.loc.x <= 0 and robot.loc.y >= 0:
+      if abs(tan(robot.orientation) - y/x) < 0.1 and robot.orientation >= 270 and robot.orientation <= 360:
+        commands.setWalkVelocity(0.5, 0, 0) 	      
+      else:
+	commands.setWalkVelocity(0, 0, -0.25)
+
+    elif robot.loc.x <= 0 and robot.loc.y <= 0:
+      if abs(tan(robot.orientation) - y/x) < 0.1 and robot.orientation >= 0 and robot.orientation <= 90:
+        commands.setWalkVelocity(0.5, 0, 0) 	      
+      else:
+	commands.setWalkVelocity(0, 0, -0.25)
+
+    elif robot.loc.x >= 0 and robot.loc.y <= 0:
+      if abs(tan(robot.orientation) - y/x) < 0.1 and robot.orientation >= 90 and robot.orientation <= 180:
+        commands.setWalkVelocity(0.5, 0, 0) 	      
+      else:
+	commands.setWalkVelocity(0, 0, -0.25)
+
+
+class WalkNode(Node):
+  def run(self):
+    robot = core.world_objects.getObjPtr(core.robot_state.WO_SELF)
+    print robot.loc.x
+    print robot.loc.y
+    print robot.orientation
+    print "one"
+   
+    if self.getTime() > 30.0:
+      commands.stand()
+      self.postSuccess()
+
+
+
+################################################################################	
+
+def rand():
+  t = int(time.time() * 1000) % 1000
+  return t
+
+class Choices:
   Left = 0
   Right = 1
-  Middle = 2
+  Forward = 2
+  NumChoices = 3
 
-class LocateBallNode(Node):
-  def __init__(self, fromTop):
-    super(LocateBallNode, self).__init__()
-    self.fromTop = fromTop
-  
+class ChooseNode(Node):
+  _choices = 0
   def run(self):
-    ball = core.world_objects.getObjPtr(core.WO_BALL)
-    if ball.seen:
-      core.speech.say("I see the ball")
-    else:
-      core.speech.say("Where is the ball")
-    choice = BallLocation.Middle
-    if ball.fromTopCamera == self.fromTop:
-      print ball.imageCenterX, ball.imageCenterY
-      if ball.imageCenterX < 150:
-        choice = BallLocation.Left
-      elif ball.imageCenterX > 170:
-        choice = BallLocation.Right
-      else:
-        choice = BallLocation.Middle
-    self.postSignal(choice)  
-
-class BlueWallLocation:
-  FarRight = 0
-  FarLeft = 1
-  Near = 2
-
-class LocateBlueWallNode(Node):
-  def run(self):
-    goal = core.world_objects.getObjPtr(core.WO_OPP_GOAL)
-    print goal.imageCenterX, goal.imageCenterY, goal.radius
-    if self.getTime() > 0.5:
-      choice = BlueWallLocation.Near
-      if goal.fromTopCamera:
-        if goal.radius < 0.5:
-          if goal.imageCenterX < 160:
-            choice = BlueWallLocation.FarLeft
-          else:
-            choice = BlueWallLocation.FarRight
-        else:
-          choice = BlueWallLocation.Near
+    if ChooseNode._choices > 10:
+      self.poseCompletion()
+      return
+    if self.getTime() > 4.0:
+      choice = (rand() % Choices.NumChoices)
       self.postSignal(choice)
+      ChooseNode._choices += 1
+
+class TurnLeftNode(Node):
+  def run(self):
+    commands.setWalkVelocity(0, 0, .25)
+    if self.getTime() > 4.0:
+      commands.stand()
+      self.postSuccess()
+
+class TurnRightNode(Node):
+  def run(self):
+    commands.setWalkVelocity(0, 0, -.25)
+    if self.getTime() > 4.0:
+      commands.stand()
+      self.postSuccess()
 
 class SpeakNode(Node):
   def __init__(self, phrase):
@@ -84,13 +132,19 @@ class SpeakNode(Node):
     if self.getTime() > 4.0:
       self.postSuccess()
 
+class WalkNode(Node):
+  def run(self):
+    commands.setWalkVelocity(.5, 0, 0)
+    if self.getTime() > 2.0:
+      commands.stand()
+      self.postSuccess()
+
 class SitNode(Node):
   def __init__(self):
     super(SitNode, self).__init__()
     self.task = pose.Sit()
 
   def run(self):
-    core.speech.say("sit")
     self.task.processFrame() 
     if self.task.finished():
       self.postCompleted()
@@ -101,91 +155,6 @@ class StandNode(Node):
     self.task = pose.Stand()
 
   def run(self):
-    core.speech.say("stand")
     self.task.processFrame()
     if self.task.finished():
       self.postCompleted()
-
-class WalkNode(Node):
-  def __init__(self, walkTime, x, y, theta):
-    super(WalkNode, self).__init__()
-    self.walkTime = walkTime
-    self.x = x
-    self.y = y
-    self.theta = theta
-      
-  def run(self):
-    print percepts.joint_angles[core.LKneePitch]  # XXX: remove
-    commands.setWalkVelocity(self.x, self.y, self.theta)
-    if self.getTime() > self.walkTime:
-      commands.stand()
-      self.postSuccess()
-
-class WalkLeftNode(Node):
-  def __init__(self, walkTime):
-    super(WalkLeftNode, self).__init__()
-    self.walkTime = walkTime
-  
-  def run(self):
-    commands.setWalkVelocity(0.1, 0, pi / 18)
-    if self.getTime() > self.walkTime:
-      commands.stand()
-      self.postSuccess()
-
-class WalkRightNode(Node):
-  def __init__(self, walkTime):
-    super(WalkRightNode, self).__init__()
-    self.walkTime = walkTime
-      
-  def run(self):
-    commands.setWalkVelocity(0.1, 0, -pi / 18)
-    if self.getTime() > self.walkTime:
-      commands.stand()
-      self.postSuccess()      
-      
-class BallLeftNode(Node):
-  def __init__(self):
-    super(BallLeftNode, self).__init__()
-
-  def run(self):
-    commands.setHeadPan(pi / 18, 0.2, True)
-    if self.getTime() > 0.3:
-      # XXX: don't use time to terminate
-      self.postSuccess()
- 
-class BallRightNode(Node):
-  def __init__(self):
-    super(BallRightNode, self).__init__()
-
-  def run(self):
-    commands.setHeadPan(-pi / 18, 0.2, True)
-    if self.getTime() > 0.3:
-      # XXX: don't use time to terminate
-      self.postSuccess()
-
-class TiltHeadNode(Node):
-  def __init__(self, tilt):
-    super(TiltHeadNode, self).__init__()
-    self.tilt = tilt
-  
-  def run(self):
-    commands.setHeadTilt(self.tilt)
-    if self.getTime() > 2.0:
-      # XXX: don't use time to terminate
-      self.postCompleted()
-
-class TurnHeadNode(Node):
-  def __init__(self, angle, turnTime, relative):
-    super(TurnHeadNode, self).__init__()
-    self.angle = angle
-    self.turnTime = turnTime
-    self.relative = relative
-  
-  def run(self):
-    core.speech.say("turn head")
-    commands.setHeadPan(self.angle, self.turnTime, self.relative)
-    if self.getTime() > self.turnTime + 10.0:
-      self.postSuccess()
-
-
-
